a7ae1b271452575e13fe0b35e654d4cd6107aa51
whals <whalsdud0131@gmail.cm>
2025-06-10 10:15:38 +0900

    class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Person {
+    String name;
+    String mbti;
+    int age;
+    double height;
+    boolean isFemale;
+
+    public void sayHello(){
+        System.out.println("반갑습니다. 저는 " + name + "입니다. 저는 " + age + "세 입니다. 키는 "+ height + "cm 입니다.");
+        System.out.println("mbti는 " + mbti + "입니다. 제가 여자인건 " + isFemale + "입니다.");
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Person person1 = new Person();
+        person1.name = "조민영";
+        person1.mbti = "INTP";
+        person1.age = 25;
+        person1.height = 164.8;
+        person1.isFemale = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        person1.sayHello();
     }
 }
