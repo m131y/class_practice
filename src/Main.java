de27997fad7e7f2ae728ab45f4f290ad82fca9a2
whals <whalsdud0131@gmail.cm>
2025-06-10 16:38:46 +0900

    Constructors

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,23 @@
 class Person {
     String name;
-    String mbti;
     int age;
-    double height;
-    boolean isFemale;
 
-    public void sayHello(){
-        System.out.println("반갑습니다. 저는 " + name + "입니다. 저는 " + age + "세 입니다. 키는 "+ height + "cm 입니다.");
-        System.out.println("mbti는 " + mbti + "입니다. 제가 여자인건 " + isFemale + "입니다.");
+    Person(String name, int age){
+        this.name = name;
+        this.age = age;
+        System.out.println(name);
+    }
+    void hello() {
+        System.out.println("hello");
     }
 }
 
-
 public class Main {
     public static void main(String[] args) {
-        Person person1 = new Person();
-        person1.name = "조민영";
-        person1.mbti = "INTP";
-        person1.age = 25;
-        person1.height = 164.8;
-        person1.isFemale = true;
+        Person person1 = new Person("gott",23);
+        Person person2 = new Person("ejwkf",22);
 
-        person1.sayHello();
+        person2.hello();
+        person1.hello();
     }
 }
